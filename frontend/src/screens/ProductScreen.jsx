e3b6664b2f0bc0d22919fdcbf0bb93ce24f64bcb
fitgeek96 <fitgeek96@gmail.com>
2024-03-29 03:07:40 +0200

    Qty & Add to cart Handler

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,18 +1,43 @@
-import React from "react";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from "react-bootstrap";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-
+import { addToCart } from "../slices/cartSlice";
 // import axios from "axios";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
+
+  const [qty, setQty] = useState();
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { data: product, isLoading, isError } = useGetProductDetailsQuery(
     productId,
   );
+
+  const addToCartHandler = () => {
+    dispatch(
+      addToCart({
+        ...product,
+        qty,
+      }),
+    );
+    navigate("/cart");
+  };
 
   // const [product, setProduct] = useState([]);
   // const [loading, setLoading] = useState(true);
@@ -64,7 +89,7 @@ const ProductScreen = () => {
               <ListGroup.Item>
                 <Row>
                   <Col>Prix:</Col>
-                  <Col>
+                  <Col className="text-center">
                     <strong>DZD {product?.price}</strong>
                   </Col>
                 </Row>
@@ -72,7 +97,7 @@ const ProductScreen = () => {
               <ListGroup.Item>
                 <Row>
                   <Col>Etat:</Col>
-                  <Col>
+                  <Col className="text-center">
                     <strong>
                       {product?.countInStock > 0
                         ? "Disponible"
@@ -81,11 +106,36 @@ const ProductScreen = () => {
                   </Col>
                 </Row>
               </ListGroup.Item>
+              {product?.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Quantité:</Col>
+                    <Col>
+                      <Form.Control
+                        as="select"
+                        value={qty}
+                        onChange={(e) => setQty(Number(e.target.value))}
+                        className="text-center p-0 m-auto w-50"
+                      >
+                        {Array.from(
+                          { length: product?.countInStock },
+                          (v, i) => (
+                            <option key={i} value={i + 1}>
+                              {i + 1}
+                            </option>
+                          ),
+                        )}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
               <ListGroup.Item className="text-center">
                 <Button
                   variant="danger text-white"
                   size="lg"
                   disabled={product?.countInStock === 0}
+                  onClick={addToCartHandler}
                 >
                   Ajouter au panier
                 </Button>
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -8,7 +8,7 @@ const addDecimals = (num, decimalPlaces = 2) => {
   // Convert the number to a string with fixed decimal places
   const fixedNum = num.toFixed(decimalPlaces);
   return parseFloat(fixedNum); // Convert the string back to a number and return
-}
+};
 
 const cartSlice = createSlice({
   name: "cart",
@@ -29,20 +29,22 @@ const cartSlice = createSlice({
       }
 
       // Calculate items price
-      state.itemPrice = addDecimals(state.cartItems.reduce(
-        (acc, item) => acc + item.price * item.qty,
-      , 0))
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0),
+      );
       // Calculate shipping price
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
       // Calculate tax price
-      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice)))
+      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice));
       // Calculate total price
       state.totalPrice = addDecimals(
-        Number(state.itemPrice) + Number(state.shippingPrice) + Number(state.taxPrice),
+        Number(state.itemsPrice) +
+          Number(state.shippingPrice) +
+          Number(state.taxPrice),
       );
 
       localStorage.setItem("cart", JSON.stringify(state));
-    }
+    },
   },
 });
 
