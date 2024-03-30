1d62b89310e647d7eef52d50a5507ace86ba5914
fitgeek96 <fitgeek96@gmail.com>
2024-03-31 01:48:20 +0200

    Add Auth middleware & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
