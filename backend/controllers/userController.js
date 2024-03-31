1c3150c255cce2ba71303a22a6ace0bc42c2f543
fitgeek96 <fitgeek96@gmail.com>
2024-03-31 04:52:11 +0300

    Add User Profile Enpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,7 +78,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("Get User Profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found!");
+  }
 });
 
 // @desc    Update User Profile
@@ -86,7 +98,28 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @access  Private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("Update User Profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found!");
+  }
 });
 
 // @desc    Get Users
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -17,7 +17,7 @@ const protect = asyncHandler(async (req, res, next) => {
   } else {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = User.findById(decoded.userId).select("-password");
+      req.user = await User.findById(decoded.userId).select("-password");
       next();
     } catch (error) {
       return res.status(401).json({
