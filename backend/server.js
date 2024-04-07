60a40428da4a7bbd3dc16013f37b07d71f9aa8cd
fitgeek96 <fitgeek96@gmail.com>
2024-04-07 17:31:01 +0300

    udpate

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -35,6 +32,20 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(__dirname + "/frontend/build"));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
